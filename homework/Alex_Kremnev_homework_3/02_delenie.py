b66baae519daac8771c222aef3ce4b1643ee9586
AlexK <915forwow@gmail.com>
2025-09-09 18:06:39 +0300

    Правка ДЗ после линтера

--- homework/Alex_Kremnev_homework_3/02_delenie.py
+++ homework/Alex_Kremnev_homework_3/02_delenie.py
@@ -1,4 +1,4 @@
 x = 4
 y = 2
 
-print(x-y/1+x*y)
+print(x - y / 1 + x * y)
--- homework/Alex_Kremnev_homework_3/04_kateti.py
+++ homework/Alex_Kremnev_homework_3/04_kateti.py
@@ -1,7 +1,7 @@
 a = 3
 b = 4
 
-hypotenuse = (a**2 + b**2) ** 0.5
+hypotenuse = (a ** 2 + b ** 2) ** 0.5
 area = 0.5 * a * b
 
 print("Гипотенуза:", hypotenuse)
