8ebaade30e3d70551f0e911a7d87066fdd78db39
AlexK <915forwow@gmail.com>
2025-09-11 17:48:11 +0300

    HW_4_правки_после_линтера_часть_2

--- homework/Alex_Kremnev_homework_4/HW4.py
+++ homework/Alex_Kremnev_homework_4/HW4.py
@@ -1,7 +1,15 @@
-my_dict = {"tuple": (1, 2, 3, 4, 5),
+my_dict = {
+    "tuple": (1, 2, 3, 4, 5),
     "list": ["aple", "first", "ss", "ew", "mm"],
-    "dict": {"name": "Alice","city": "London","color": "blue","language": "Python","year": 2025},
-    "set": {"book", "lamp", "tea", "window", 100}}
+    "dict": {
+        "name": "Alice",
+        "city": "London",
+        "color": "blue",
+        "language": "Python",
+        "year": 2025,
+    },
+    "set": {"book", "lamp", "tea", "window", 100},
+}
 
 print(my_dict["tuple"][-1])  #  выведите на экран последний элемент
 
@@ -9,7 +17,9 @@ print(my_dict["list"].append("end"))  #  добавьте в конец спис
 print(my_dict["list"].pop(1))  #  удалите второй элемент списка
 print(my_dict["list"])
 
-my_dict["dict"]["i am a tuple"] = "value"  #  добавьте элемент с ключом ('i am a tuple',) и любым значением
+my_dict["dict"][
+    "i am a tuple"
+] = "value"  #  добавьте элемент с ключом ('i am a tuple',) и любым значением
 print(my_dict["dict"])
 my_dict["dict"].pop("name")  #  удалите какой-нибудь элемент
 print(my_dict["dict"])
