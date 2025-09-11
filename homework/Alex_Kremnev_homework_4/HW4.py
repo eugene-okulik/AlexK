63556190dccc91a3034470ac6e64d242f05d66c5
AlexK <915forwow@gmail.com>
2025-09-11 17:50:48 +0300

    HW_4_правки_после_линтера_часть_3

--- homework/Alex_Kremnev_homework_4/HW4.py
+++ homework/Alex_Kremnev_homework_4/HW4.py
@@ -11,19 +11,19 @@ my_dict = {
     "set": {"book", "lamp", "tea", "window", 100},
 }
 
-print(my_dict["tuple"][-1])  #  выведите на экран последний элемент
+print(my_dict["tuple"][-1])  # выведите на экран последний элемент
 
-print(my_dict["list"].append("end"))  #  добавьте в конец списка еще один элемент
-print(my_dict["list"].pop(1))  #  удалите второй элемент списка
+print(my_dict["list"].append("end"))  # добавьте в конец списка еще один элемент
+print(my_dict["list"].pop(1))  # удалите второй элемент списка
 print(my_dict["list"])
 
 my_dict["dict"][
     "i am a tuple"
-] = "value"  #  добавьте элемент с ключом ('i am a tuple',) и любым значением
+] = "value"  # добавьте элемент с ключом ('i am a tuple',) и любым значением
 print(my_dict["dict"])
-my_dict["dict"].pop("name")  #  удалите какой-нибудь элемент
+my_dict["dict"].pop("name")  # удалите какой-нибудь элемент
 print(my_dict["dict"])
 
-my_dict["set"].add("dalas")  #  добавьте новый элемент в множество
-my_dict["set"].pop()  #  удалите элемент из множества
+my_dict["set"].add("dalas")  # добавьте новый элемент в множество
+my_dict["set"].pop()  # удалите элемент из множества
 print(my_dict["set"])
