e9e2b077e9af1e932969d7a2f9a02855e4aa5783
AlexK <915forwow@gmail.com>
2025-09-28 14:03:53 +0300

    Выполнено ДЗ номер 5, готовы все 3 задания

---
+++ homework/Alex_Kremnev_homework_5/HW5.py
@@ -0,0 +1,55 @@
+line = "Число: 99"
+first_ind = line.index(":") + 2
+print(first_ind)
+
+
+#  задание 1
+person = ["John", "Doe", "New York", "+1372829383739", "US"]
+name, last_name, city, phone, country = person
+print(name, last_name, city, phone, country)
+
+#  задание 2
+line = "результат операции: 42"
+position_1 = line.index(":") + 2
+print(position_1)
+num_str = line[position_1:]
+print(num_str)
+print(type(num_str))
+number = int(num_str)
+print(number)
+print(type(number))
+summa = number + 10
+print(summa)
+
+line = "результат операции: 514"
+position_1 = line.index(":") + 2
+print(position_1)
+num_str = line[position_1:]
+print(num_str)
+print(type(num_str))
+number = int(num_str)
+print(number)
+print(type(number))
+summa = number + 10
+print(summa)
+
+line = "результат работы программы: 9"
+position_1 = line.index(":") + 2
+print(position_1)
+num_str = line[position_1:]
+print(num_str)
+print(type(num_str))
+number = int(num_str)
+print(type(number))
+summa = number + 10
+print(summa)
+
+
+
+#  задание 3
+students = ["Ivanov", "Petrov", "Sidorov"]
+subjects = ["math", "biology", "geography"]
+print(f"Students {', '.join(students)} study these subjects: {', '.join(subjects)}")
+
+
+
